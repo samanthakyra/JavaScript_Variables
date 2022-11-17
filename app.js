26334b67765a513a62c901e046c09b2c7504d757
samanthakyra <samanthakyramendoza@gmail.com>
2022-11-16 19:16:40 -0800

    final commit

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+ const firstName = "Kash";
+ let lastName = "Chavez";
+ var age = 29;
 
-// Exercise 2
+ lastName = "Shabooya";
+ age = 25;
 
-// Exercise 3
+ let language = "JavaScript";
+ let createdYear = 1997;
+ let isCaseSensitive = true;
+
+ console.log(`${language} was released in ${createdYear}.`);
+
+ let product = "Jacket";
+ let price = 19.99;
+ let isOnSale = false;
+ let salePercentage =  15;
+ let stock = 0;
+ let inStock = false;
+ let selectedSize = "M";
+
+ console.log(`${product} in size ${selectedSize} is ${price} with ${stock} in stock.`);
+
+ let title = "Name of the Wind";
+ let author = "Patrick Rothfuss";
+ let pageCount = 722;
+ let bookmark = 456;
+ let hasRead = true;
+
+ console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`);
